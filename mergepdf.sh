4a83b0c63c9ccc5a4517c362ca2fa1276471a633
tuxflo <tuxflo@users.noreply.github.com>
2016-03-07 22:08:28 +0100

    add simple sleep time to wait until files finished

--- mergepdf.sh
+++ mergepdf.sh
@@ -11,24 +11,8 @@ fi
 
 if [[  "$1" != *_o.pdf && "$1" != *_e.pdf  ]]; then
   #no multipage pdf file, move directly to Output folder
-  until err_str=$(lsof $1 2>&1 >/dev/null); do
-    echo "lsof file check"
-    if [ -n "$err_str" ]; then
-      # lsof printed an error string, file may or may not be open
-      echo "lsof: $err_str" >&2
-
-      # tricky to decide what to do here, you may want to retry a number of times,
-      # but for this example just break
-      break
-    fi
-
-    # lsof returned 1 but didn't print an error string, assume the file is open
-    sleep 1
-  done
-
-  if [ -z "$err_str" ]; then
-    # file has been closed, move it
-    mv $1 $OUTPUT
+  sleep 12
+  mv $1 $OUTPUT
   fi
   exit
 fi
@@ -45,6 +29,7 @@ fi
 stringMerged=${stringOdd/%_o.pdf/_merged.pdf}
 
 if [[ -f $stringOdd && -f $stringEven ]]; then
+  sleep 5
   pdftk A=$stringOdd B=$stringEven shuffle A Bend-1 output $OUTPUT/$stringMerged
   rm  -f $stringOdd $stringEven
   sleep 2 #needed when copying multiple files into the Input folder
