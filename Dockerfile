f3565463216e54eaae3dd1d9bfae50ada6218484
tuxflo <flo.hennig@gmail.com>
2016-03-07 12:40:48 +0100

    initial commit add Dockerfile and mergepdf.sh

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM ruudk/pdftk
+
+MAINTAINER tuxflo
+
+RUN apt-get update & apt-get install -y incron
+
+ADD ./mergepdf.sh /opt/mergepdf.sh
+RUN chmod a+x /opt/mergepdf.sh
+
+RUN adduser --disabled-password --gecos '' r && adduser r sudo && echo '%sudo ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
+RUN echo r >> /etc/incron.allow
+USER r
+
+RUN cd /home/r && incrontab -l > mycron && echo '/srv/input IN_CREATE /opt/mergepdf.sh $#' >> mycron && incrontab mycron && rm mycron
---
+++ mergepdf.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+INPUT=/srv/input
+OUTPUT=/srv/output
+
+cd $INPUT
+
+if [[ $1 != *.pdf ]]; then
+  #not a pdf file, do nothing
+  exit
+fi
+
+if [[  "$1" != *_o.pdf && "$1" != *_e.pdf  ]]; then
+  #no multipage pdf file, move directly to Output folder
+  mv $1 $OUTPUT
+  exit
+fi
+
+if [[ "$1" == *_o.pdf ]]; then
+  stringOdd=$(basename "$1")
+  stringEven=${1/%_o.pdf/_e.pdf}
+else
+  #even file created first for some reason
+  stringOdd=${1/%_e.pdf/_o.pdf}
+  stringEven=$(basename "$1")
+fi
+
+stringMerged=${stringOdd/%_o.pdf/_merged.pdf}
+
+if [[ -f $stringOdd && -f $stringEven ]]; then
+  pdftk A=$stringOdd B=$stringEven shuffle A Bend-1 output $OUTPUT/$stringMerged
+  rm $stringOdd $stringEven
+  sleep 2 #needed when copying multiple files into the Input folder
+  exit
+fi
+
