61cc78bb4068565475bb51d2b82bf9478963c15a
tuxflo <flo.hennig@gmail.com>
2016-03-08 10:30:40 +0100

    switch from lsof to rsync to check finished file

--- mergepdf.sh
+++ mergepdf.sh
@@ -11,9 +11,14 @@ fi
 
 if [[  "$1" != *_o.pdf && "$1" != *_e.pdf  ]]; then
   #no multipage pdf file, move directly to Output folder
-  sleep 12
-  mv $1 $OUTPUT
-  fi
+  #sleep 25
+  RC=1 
+  while [[ $RC -ne 0 ]]
+  do
+    rsync -a $1 $OUTPUT
+    RC=$?
+  done
+  rm -f $1
   exit
 fi
 
