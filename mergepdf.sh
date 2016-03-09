f8cfe0e3cf644bac626c3fe026b33f7b527d0568
tuxflo <flo.hennig@gmail.com>
2016-03-09 10:25:40 +0100

    add more sleep time for large documents

--- mergepdf.sh
+++ mergepdf.sh
@@ -11,13 +11,13 @@ fi
 
 if [[  "$1" != *_o.pdf && "$1" != *_e.pdf  ]]; then
   #no multipage pdf file, move directly to Output folder
-  #sleep 40
   TMP=0
   until [ $TMP -eq $(stat -c %s $1) ]
   do
     TMP=$(stat -c %s $1)
     sleep 5
   done
+  sleep 25
   mv $1 $OUTPUT
   exit
 fi
@@ -34,6 +34,7 @@ fi
 stringMerged=${stringOdd/%_o.pdf/_merged.pdf}
 
 if [[ -f $stringOdd && -f $stringEven ]]; then
+  sleep 15
   pdftk A=$stringOdd B=$stringEven shuffle A Bend-1 output $OUTPUT/$stringMerged
   rm  -f $stringOdd $stringEven
   sleep 2 #needed when copying multiple files into the Input folder
