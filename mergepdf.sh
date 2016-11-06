144cf742f19bb303e3c0710b9690a507ddc339ea
tuxflo <tuxflo@users.noreply.github.com>
2016-11-06 21:25:18 +0100

    switch from loops to inotifywait
    using inotfywait to check if file is finished

--- mergepdf.sh
+++ mergepdf.sh
@@ -9,51 +9,49 @@ if [[ $1 != *.pdf ]]; then
   exit
 fi
 
+if [[  "$1" == *_merged.pdf ]]; then
+  echo "self created pdf, do nothing"
+  exit
+fi
+
 if [[  "$1" != *_o.pdf && "$1" != *_e.pdf  ]]; then
   #no multipage pdf file, copy directly to Output folder
-  echo "detected non mutlipage file"
-  TMP=0
-  until [ $TMP -eq $(stat -c %s $1) ]
-  do
-    TMP=$(stat -c %s $1)
-    sleep 5
-  done
-  sleep 25
-  echo "copied to output"
+  echo "$(date +%F-%T) detected non mutlipage file"
+  inotifywait -e close $1
+  sleep 3
+  echo "$(date +%F-%T) copied to output"
   cp $1 $OUTPUT
   sleep 3
-  mv --backup=t $1 $1\_bkp
-  echo "moved to $1_bkp"
+  mv --backup=t $1 $1\_$(date +%F-%T)
+  echo "moved to $1_$(date +%F-%T)"
   exit
 fi
 
 if [[ "$1" == *_o.pdf ]]; then
-  echo "odd file detected"
+  echo "$(date +%F-%T) odd file detected"
   stringOdd=$(basename "$1")
   stringEven=${1/%_o.pdf/_e.pdf}
 else
   #even file created first for some reason
-  echo "even file detected"
+  echo "$(date +%F-%T) even file detected"
   stringOdd=${1/%_e.pdf/_o.pdf}
   stringEven=$(basename "$1")
 fi
 
 stringMerged=${stringOdd/%_o.pdf/_merged.pdf}
-  echo "merged sting $stringMerged"
+  echo "merged string $stringMerged"
 
 if [[ -f $stringOdd && -f $stringEven ]]; then
-  sleep 15
+  inotifywait -e close $stringOdd $stringEven
+  sleep 1
   echo "executing pdftk"
   pdftk A=$stringOdd B=$stringEven shuffle A Bend-1 output $stringMerged
-  sleep 3
+  sleep 1
   cp $stringMerged $OUTPUT/
-  mv $stringMerged $stringMerged\_bkp
+  mv $stringMerged $stringMerged\_$(date +%F-%T)
   echo "removing files"
-  #rm  -f $stringOdd $stringEven
-  mv $stringOdd $stringOdd\_bkp
-  mv $stringEven $stringEven\_bkp
+  rm  -f $stringOdd $stringEven
   sleep 2 #needed when copying multiple files into the Input folder
   echo "done..."
   exit
 fi
-
